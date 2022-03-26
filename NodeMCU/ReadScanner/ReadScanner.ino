0a43c7900af703f56b8057a3a6e75972048c5aeb
Cart To Go <98914245+GP2022KSU@users.noreply.github.com>
2022-03-26 23:28:32 +0300

    Add files via upload
    Create Arabic characters to display the price and total price in Arabic.

--- NodeMCU/ReadScanner/ReadScanner.ino
+++ NodeMCU/ReadScanner/ReadScanner.ino
@@ -10,22 +10,19 @@
 #include <FirebaseESP8266.h>
 #endif
 #include <addons/TokenHelper.h>
-
 #include <addons/RTDBHelper.h>
 
+
+//Wifi connection
 #define WIFI_SSID "Alhomaidhi"
 #define WIFI_PASSWORD "0504356565"
 
-//For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
-
-
+//Firebase connection
 #define API_KEY "AIzaSyCFoxsSG6CUrgi5DuiFz6Ph1v2kjdoDbcg"
-
-
 #define DATABASE_URL "carttogo-411c2-default-rtdb.europe-west1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
-
 #define USER_EMAIL "reemaazaid@gmail.com"
 #define USER_PASSWORD "112233"
+
 
 //Define Firebase Data object
 FirebaseData fbdo;
@@ -33,6 +30,7 @@ FirebaseAuth auth;
 FirebaseConfig config;
 bool signupOK = false;
 unsigned long sendDataPrevMillis = 0;
+
 SoftwareSerial mySerial(14, 12); // RX, TX
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
@@ -63,7 +61,7 @@ void setup() {
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASSWORD;
   config.database_url = DATABASE_URL;
-    if (auth.user.email==USER_EMAIL&&auth.user.password==USER_PASSWORD) {
+  if (auth.user.email == USER_EMAIL && auth.user.password == USER_PASSWORD) {
     Serial.println("ok");
     signupOK = true;
   }
@@ -77,12 +75,15 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("Connected");
   delay(3000);
+  
+          //Need to be translated into Arabic later
   lcd.setCursor(0, 0);
-  lcd.print("Start by scan");
+  lcd.print("Start by scanning");
   lcd.setCursor(0, 1);
- lcd.print("a product");
+  lcd.print("a product");
 
 }
+float price = 0;
 float total = 0;
 void loop()
 {
@@ -91,13 +92,14 @@ void loop()
   FirebaseJson json;
   FirebaseJsonData price;
   FirebaseJsonData name1;
-  if (Firebase.ready()==1 && signupOK) {
+  if (Firebase.ready() == 1 && signupOK) {
     if (mySerial.available()) { //Check if there is Incoming Data in the Serial Buffer
       while (mySerial.available()) {  //Keep reading Byte by Byte from the Buffer till the Buffer is empty
         readBarcode = mySerial.read(); //Read 1 Byte of data and store it in a character variable
         barcode = barcode + readBarcode;
         delay(5); // A small delay
       }
+
       String barcode1 = "/Products/" + barcode;
       Serial.println("Path barcode: " + barcode1);
       if (Firebase.RTDB.getJSON(&fbdo, barcode1)) {
@@ -106,19 +108,60 @@ void loop()
         json.get(price, "/Price");
         json.get(name1, "/Name");
         if (price.success && name1.success)
-        { lcd.clear();
+        {
+          lcd.clear();
           total = total + price.to<float>();
-          Serial.println("Item: " + name1.to<String>() + " Price: " + price.to<int>() + " Total: " + String(total));
+          Serial.println(
+            "المنتج: " + name1.to<String>()
+            + " السعر: " + price.to<float>()
+            + "الاجمالي: " + String(total));
+
+          byte al [8] = {5, 5, 5, 5, 29, 0, 0, 0}; //ال
+          byte s [8] = {0, 21, 21, 21, 31, 0, 0, 0}; //س
+          byte aen [8] = {0, 0, 14, 14, 31, 0, 0, 0}; //ع
+          byte r [8] = {0, 0, 0, 0, 3, 4, 8, 0}; //ر
+          byte a [8] = {4, 4, 4, 4, 7, 0, 0, 0};  //أ
+          byte jem [8] = {0, 0, 12, 3, 30, 0, 4, 0}; //جـ
+          byte m [8] = {0, 0, 0, 0, 31, 10, 10, 14}; //م
+          byte ly [8] = {1, 1, 1, 1, 23, 28, 0, 12}; //لي
+
+          lcd.createChar(0, al);
+          lcd.createChar(1, s);
+          lcd.createChar(2, aen);
+          lcd.createChar(3, r);
+          lcd.createChar(4, a);
+          lcd.createChar(5, jem);
+          lcd.createChar(6, m);
+          lcd.createChar(7, ly);
+
+          lcd.home();
+          lcd.setCursor(15, 0);
+          lcd.rightToLeft();
+          lcd.write(0);
+          lcd.write(1);
+          lcd.write(2);
+          lcd.write(3);
+          lcd.setCursor(11, 0);
+          lcd.print(":");
           lcd.setCursor(0, 0);
-          lcd.print("Price:" + price.to<String>());
-          //lcd.print(name1.to<String>());
-          //lcd.setCursor(1, 1);
-          //lcd.print("Price:" + price.to<String>());
+          lcd.print(price.to<String>());
+
+          lcd.setCursor(15, 1);
+          lcd.write(0);
+          lcd.write(4);
+          lcd.write(5);
+          lcd.write(6);
+          lcd.write(4);
+          lcd.write(7);
+          lcd.leftToRight();
+          lcd.setCursor(9, 1);
+          lcd.print(":");
           lcd.setCursor(0, 1);
-          lcd.print("Total:" + String(total));
+          lcd.print(String(total));
         }
       }
       else {
+        //Need to be translated into Arabic later
         Serial.println(fbdo.errorReason().c_str());
         lcd.clear();
         lcd.setCursor(0, 0);
