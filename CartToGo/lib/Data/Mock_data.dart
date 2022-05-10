923e12cab769d170079f04612c106c6b7afe7845
saraabanmi <439201045@student.ksu.edu.sa>
2022-05-10 07:11:30 +0300

    Mock_data file changed

--- CartToGo/lib/Data/Mock_data.dart
+++ CartToGo/lib/Data/Mock_data.dart
@@ -1,5 +1,6 @@
 import 'package:carttogo/Users/Products.dart';
 
+//new codeeee
 List<Product> products = [
   Product(name: 'حليب مراعي  ', size: "20ملم", price: "4ريل", quantity: "26"),
   Product(name: 'مياه بيرين  ', size: "م200", price: "٢ريال", quantity: "1000"),
