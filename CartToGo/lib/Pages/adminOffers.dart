525889028f55ee7522105d0f6583f03a53bd697a
Haya <haya55661@hotmail.com>
2022-10-22 00:07:08 +0300

    admin offer appbar

--- CartToGo/lib/Pages/adminOffers.dart
+++ CartToGo/lib/Pages/adminOffers.dart
@@ -1,6 +1,7 @@
-
+import 'package:carttogo/Pages/welcomePage.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import '../main.dart';
 
 class AdminOffers extends StatefulWidget {
   AdminOffers({Key? key}) : super(key: key);
@@ -11,10 +12,114 @@ class AdminOffers extends StatefulWidget {
 class _AdminOffersState extends State<AdminOffers> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: appColor),
+        backgroundColor: Colors.white24,
+        centerTitle: true,
+        elevation: 0,
+        title: const Text("العروض",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            )),
+
+        // logout button
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(3),
+                    textStyle: MaterialStateProperty.all(const TextStyle(
+                        fontSize: 14,
+                        fontFamily: 'CartToGo',
+                        fontWeight: FontWeight.bold)),
+                    fixedSize: MaterialStateProperty.all(const Size(70, 10)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0))),
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                    foregroundColor: MaterialStateProperty.all(Colors.white)),
+                onPressed: () async {
+                  _showMyDialog();
+                },
+                child: const Text('خروج')),
+          ),
+        ],
+      ),
+    );
   }
 
-
-
+  //logout dialog, to ensure that the admin want to log out or not
+  void _showMyDialog() async {
+    return showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return Directionality(
+              textDirection: TextDirection.rtl,
+              child: Dialog(
+                  elevation: 0,
+                  backgroundColor: const Color(0xffffffff),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    const SizedBox(height: 15),
+                    const Text("هل تريد تسجيل الخروج؟",
+                        style: const TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    const SizedBox(height: 15),
+                    const Divider(
+                      height: 1,
+                      color: Colors.black,
+                    ),
+                    Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: InkWell(
+                            highlightColor: Colors.grey[200],
+                            onTap: () async {
+                              await FirebaseAuth.instance.signOut();
+                              print(
+                                  "UID: ${FirebaseAuth.instance.currentUser?.uid}");
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const WelcomePage()));
+                            },
+                            child: const Center(
+                                child: const Text("خروج",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Color(0xFFFE4A49),
+                                      fontWeight: FontWeight.bold,
+                                    ))))),
+                    const Divider(
+                      height: 1,
+                    ),
+                    Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        child: InkWell(
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: const Radius.circular(15.0),
+                              bottomRight: Radius.circular(15.0),
+                            ),
+                            highlightColor: Colors.grey[200],
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Center(
+                                child: Text("إلغاء",
+                                    style: const TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    )))))
+                  ])));
+        });
+  }
 }
