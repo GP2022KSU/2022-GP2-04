a25ba82b7d78f27550d3968e65b946dbb7b767b4
Haya <haya55661@hotmail.com>
2022-10-26 00:12:28 +0300

    update navigation

---
+++ CartToGo/lib/Pages/Shopper/Lists.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Lists extends StatefulWidget {
+  @override
+  State<Lists> createState() => _ListsState();
+}
+
+class _ListsState extends State<Lists> {
+ 
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold();
+  }}
