05a7da3f1958da9ca890493270f5f147c484a3d3
Haya <haya55661@hotmail.com>
2022-11-04 18:30:33 +0300

    fix bugs

--- CartToGo/lib/Pages/Admin/addNewProduct.dart
+++ CartToGo/lib/Pages/Admin/addNewProduct.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
